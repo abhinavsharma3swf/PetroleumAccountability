148430c3a8886a12b12118e9b9008aa6ffb7d1e6
Abhinav Sharma <abhinav.sharma3@hotmailcom>
2025-05-14 15:04:08 -0500

    Initial Commit for Accountability Project

---
+++ PetroleumAccountability/src/test/java/com/bridge/example/petroleumaccountability/PetroleumAccountabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridge.example.petroleumaccountability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetroleumAccountabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
