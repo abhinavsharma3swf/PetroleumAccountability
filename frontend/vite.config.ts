d47ed350e20e096590fa18b9411c2b6364f123e1
Abhinav Sharma <abhinav.sharma3@hotmailcom>
2025-05-15 14:47:35 -0500

    Frontend Started

---
+++ frontend/vite.config.ts
@@ -0,0 +1,30 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port:3000,
+    strictPort: true,
+    hmr:{
+      clientPort: 3000,
+    },
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
+  plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    css: true,
+  },
+})
